a3c2d0a19c7b94e64a959e1e78785b3a6db27c75
Alessandro Lima <alessandroslima@hotmail.com>
2024-05-27 22:40:07 -0700

    Updates

---
+++ precache-manifest.407eb7f9a0419fc9eecea443d102a777.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d235801725d2eb5405204be60c8d80",
+    "url": "/kardium-automation-engineer/index.html"
+  },
+  {
+    "revision": "52cc05e941008b629e83",
+    "url": "/kardium-automation-engineer/static/css/main.a440b89e.chunk.css"
+  },
+  {
+    "revision": "58612b70808ba2d3edec",
+    "url": "/kardium-automation-engineer/static/js/2.1ffcc778.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/kardium-automation-engineer/static/js/2.1ffcc778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52cc05e941008b629e83",
+    "url": "/kardium-automation-engineer/static/js/main.2ae70445.chunk.js"
+  },
+  {
+    "revision": "3fd9f7bd33c6ae4155aa",
+    "url": "/kardium-automation-engineer/static/js/runtime-main.5e7afe81.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/kardium-automation-engineer/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/kardium-automation-engineer/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/kardium-automation-engineer/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "2c7701aae07adecdb0a7e336dff08def",
+    "url": "/kardium-automation-engineer/static/media/html-css-javascript.2c7701aa.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/kardium-automation-engineer/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "90616a5dc9acdcce7158addc89620052",
+    "url": "/kardium-automation-engineer/static/media/mern-stack.90616a5d.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/kardium-automation-engineer/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/kardium-automation-engineer/static/media/react.f370c1e0.png"
+  }
+]);
